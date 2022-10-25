94f722e0380c468f77746187b672036eb9d6a543
mahbubh0ssain <mahbubju48@gmail.com>
2022-10-25 11:04:38 +0600

    single course added

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const courses = require("./data/course-category.json");
+const courseCategory = require("./data/course-category.json");
+const courses = require("./data/courses.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("You are in the LEARN SHINE server.");
 });
 
+app.get("/course-category", (req, res) => {
+  res.send(courseCategory);
+});
+
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((info) => info.id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {
